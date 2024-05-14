583f73d0f3e528bf1de957c12995e20902f8ff84
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2024-05-14 17:20:12 +0000

    Detailed test cases for:
    -`filter_am_table()`
    -`filter_cv_table()`
    - `prepare_data_for_plot_clinvar()`
    - Comprehensive `plot_clinvar()`

--- tests/testthat/test-plot_clinvar.R
+++ tests/testthat/test-plot_clinvar.R
@@ -1,92 +1,147 @@
+test_that("filter_am_table() works", {
+
+    ## Test case when am_table is missing
+    res <- suppressMessages(filter_am_table(uID = "P37023"))
+
+    expect_message(filter_am_table(uID = "P37023"))
+    expect_equal(res |> nrow(), 9557L)
+
+    ## Test case when am_table is provided by user
+    am_data <- data.frame(uniprot_id = c("A", "A", "A", "A", "A", "B"),
+                          protein_variant = c("M1A", "M2C",
+                                              "R3L", "K4L", "G5L", "F50G"),
+                          am_pathogenicity = c(0.2, 0.8, 0.5, 0.2, 0.4, 0.6),
+                          am_class = c("benign", "pathogenic", "ambiguous",
+                                       "benign", "ambiguous", "pathogenic"))
+
+    res <- filter_am_table(uID = "A", am_data)
+
+    expect_identical(unique(res$uniprot_id), "A")
+    expect_equal(res |> nrow(), 5L)
+
+    ## Test case when bad am_table is given i.e uniprotID with no hits
+    expect_error(filter_am_table(uID = "C", am_table = am_data),
+                 paste0("No AlphaMissense information found for the protein ",
+                 "accession. Check that the UniProt ID is correct.")
+                 )
+})
+
+test_that("filter_cv_table() works", {
+
+    ## Test case when cv_table is missing
+    res <- suppressMessages(filter_cv_table(uID = "P37023"))
+
+    expect_message(filter_cv_table(uID = "P37023"))
+    expect_equal(res |> nrow(), 113L)
+
+    ## Test case when cv_table is provided by user
+    cv_data <- data.frame(uniprot_id = c("A", "A", "A", "A"),
+                          protein_variant = c("M1A", "M2C", "G5L", "P10R"),
+                          cv_class = c(1, 0, 0, 0))
+
+    res <- filter_cv_table(uID = "A", cv_table = cv_data)
+
+    expect_identical(unique(res$uniprot_id), "A")
+    expect_equal(res |> nrow(), 4L)
+
+    ## Test case when bad cv_table is given i.e uniprotID with no hits
+    expect_error(filter_cv_table(uID = "C", cv_table = cv_data),
+                 paste0("No ClinVar information found for the protein ",
+                        "accession. Check that the UniProt ID is correct.")
+    )
+})
+
 test_that("prepare_data_for_plot_clinvar() works", {
     ## All the data munging happens within this function
     am_data <- data.frame(uniprot_id = c("A", "A", "A", "A", "A", "B"),
                           protein_variant = c("M1A", "M2C",
                                               "R3L", "K4L", "G5L", "F50G"),
-                          am_pathogenicity = c(0.25, 0.89, 0.5, 0.2, 0.40, 0.60),
+                          am_pathogenicity = c(0.2, 0.8, 0.5, 0.2, 0.4, 0.6),
                           am_class = c("benign", "pathogenic", "ambiguous",
                                        "benign", "ambiguous", "pathogenic"))
 
-    ## TODO: Add demo data for cv_class = 1
     cv_data <- data.frame(uniprot_id = c("A", "A", "A", "A"),
-                      protein_variant = c("M1A", "M2C", "G5L", "P10R"),
-                      cv_class = c(0, 0, 0, 0))
+                          protein_variant = c("M1A", "M2C", "G5L", "P10R"),
+                          cv_class = c(1, 0, 0, 0))
 
     res <- prepare_data_for_plot_clinvar(
         am_table = am_data,
         cv_table = cv_data
     )
 
+    ## Amino acid positions extracted correctly
+    pos <- c(3, 4, 50, 2, 5, 1)
+    expect_identical(res |> pull(aa_pos), pos)
+
+    ## All colnames are correctly present
     col_names <- c("uniprot_id", "protein_variant", "am_pathogenicity",
                    "am_class", "aa_pos", "cv_class", "code_color",
                    "max", "min")
 
-    ## All colnames are correctly present
     expect_true(all(names(res) %in% col_names))
 
     ## There are only 3 am_class types - ambiguous, benign, pathogenic
     expect_equal(res |> distinct(am_class) |> nrow(), 3L)
 
-    ## TODO: check code_color in the res and make they are valid
+    ## There are only 5 code_color types - check assigned correctly
+    color_names <- c("AM ambiguous", "AM benign", "AM pathogenic",
+                     "CV benign", "CV benign", "CV pathogenic")
 
-    ## TODO: check max and min for pathogenecity
+    expect_identical(res |> select(code_color) |> pull() |> as.vector(),
+                     color_names)
 
-    ## TODO: check that returned data frame is "grouped"
+    ## Check max and min for pathogenicity were calculated correctly
+    path_scores <- c(0.6, 0.8, 0.2, 0.2)
+
+    expect_identical(res |> filter(am_class == "pathogenic") |>
+                         pull(min) |> unique(), path_scores[1])
+    expect_identical(res |> filter(am_class == "pathogenic") |>
+                         pull(max) |> unique(), path_scores[2])
+    expect_identical(res |> filter(am_class == "benign") |>
+                         pull(min) |> unique(), path_scores[3])
+    expect_identical(res |> filter(am_class == "benign") |>
+                         pull(max) |> unique(), path_scores[4])
 })
-
-
-## TODO: Fill these up
-test_that("filter_am_table() works", {
-
-    ## Write test case when am_table is missing
-
-    ## Write test case when bad am_table is given i.e
-    ## 1. uniprotID with no hits
-
-    ## Write test case when am_table is user provided
-    ## ex: filter to a subset of the aa_substitutions table
-
-})
-
-
-## TODO: Fill these up
-test_that("filter_cv_table() works", {
-
-    ## Write test case when cv_table is missing
-
-    ## Write test case when bad cv_table is given i.e
-    ## 1. uniprot ID with no hits
-
-    ## Write test case when cv_table is user provided
-    ## ex: filter to a subset of the clinvar data table
-
-})
-
-
 
 test_that("'plot_clinvar()' works", {
     ## Create example dataframes for alphamissense and clinvar data
-    df1 <- data.frame(uniprot_id = c("A", "A", "A", "A", "A", "B"),
-                      protein_variant = c("M1A", "M2C",
-                                          "R3L", "K4L", "G5L", "F50G"),
-                      am_pathogenicity = c(0.25, 0.89, 0.5, 0.2, 0.40, 0.60),
-                      am_class = c("benign", "pathogenic", "ambiguous",
-                                   "benign", "ambiguous", "pathogenic"))
+    am_data <- data.frame(uniprot_id = c("A", "A", "A", "A", "A", "B"),
+                          protein_variant = c("M1A", "M2C",
+                                              "R3L", "K4L", "G5L", "F50G"),
+                          am_pathogenicity = c(0.2, 0.8, 0.5, 0.2, 0.4, 0.6),
+                          am_class = c("benign", "pathogenic", "ambiguous",
+                                       "benign", "ambiguous", "pathogenic"))
 
-    df2 <- data.frame(uniprot_id = c("A", "A", "A", "A"),
-                      protein_variant = c("M1A", "M2C", "G5L", "P10R"),
-                      cv_class = c(0, 0, 0, 0))
+    cv_data <- data.frame(uniprot_id = c("A", "A", "A", "A"),
+                          protein_variant = c("M1A", "M2C", "G5L", "P10R"),
+                          cv_class = c(1, 0, 0, 0))
 
-    # Call the function with example input
+    ## Call the function with example input
     plot <- plot_clinvar(uniprotId = "A",
-                         alphamissense_table = df1,
-                         clinvar_table = df2)
+                         alphamissense_table = am_data,
+                         clinvar_table = cv_data)
 
-    # check plot axes
+    ## Check aesthetics values are assigned correctly to groups
+    stroke_vec <- c(0.0, 0.0, 1.5, 1.5, 1.5)
+    expect_identical(ggplot_build(plot)$data[[1]]$stroke, stroke_vec)
+
+    shape_vec <- c(19, 19, 21, 21, 21)
+    expect_identical(ggplot_build(plot)$data[[1]]$shape, shape_vec)
+
+    size_vec <- c(2, 2, 4, 4, 4)
+    expect_identical(ggplot_build(plot)$data[[1]]$size, size_vec)
+
+    color_vec <- c("gray", "#89d5f5", "black", "black", "black")
+    expect_identical(ggplot_build(plot)$data[[1]]$colour, color_vec)
+
+    fill_vec <- c("gray", "#89d5f5", "#007cb0", "#007cb0", "#c70606")
+    expect_identical(ggplot_build(plot)$data[[1]]$fill, fill_vec)
+
+    ## check plot axes
     expect_identical(plot$labels$y, "AlphaMissense score")
     expect_identical(plot$labels$x, "amino acid position")
 
-    # check plot layers
+    ## check plot layers
     expect_in(class(plot$layers[[1]]$geom),
               c("GeomPoint", "Geom", "ggproto", "gg"))
     expect_in(class(plot$layers[[2]]$geom),
@@ -94,24 +149,13 @@ test_that("'plot_clinvar()' works", {
     expect_in(class(plot$layers[[3]]$geom),
               c("GeomHline", "Geom", "ggproto", "gg"))
 
-    # check plot scales
+    ## check plot scales exists in aesthetics
     expect_in(plot$scales$scales[[1]]$aesthetics, "stroke")
     expect_in(plot$scales$scales[[2]]$aesthetics, "shape")
     expect_in(plot$scales$scales[[3]]$aesthetics, "size")
     expect_in(plot$scales$scales[[4]]$aesthetics, "colour")
     expect_in(plot$scales$scales[[5]]$aesthetics, "fill")
 
-    # check if the result is a ggplot object
+    ## check if the result is a ggplot object
     expect_true("ggplot" %in% class(plot))
-
-    ## TODO:
-    ## 1. Write test case where numeric uniprot ID is given
-    expect_error(plot_clinvar(uniprotId = "A"))
-
-    ## TODO
-    ## 2. Write a test case where AM table with bad colnames is given
-
-    ## TODO
-    ## 3. Write test case where CV table with bad colnames is given
-
 })
